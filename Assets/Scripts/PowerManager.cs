2985c8c02274b19684f4ab67f33bc537b88d1e12
mathieu-hub <m.vancappellen@rubika-edu.com>
2021-04-25 19:41:11 +0200

    Usage Counter Works + Some Modifications

--- Assets/Scripts/PowerManager.cs
+++ Assets/Scripts/PowerManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PowerManager : MonoBehaviour
 {
@@ -8,6 +9,11 @@ public class PowerManager : MonoBehaviour
     
     public float powerLeft;
     public float powerSink = 0.1f;
+    public int poweredObjects;
+
+    [Header("POWER COUNTER")]
+    public List<GameObject> usageCount;
+    
 
     private void Awake()
     {
@@ -16,7 +22,8 @@ public class PowerManager : MonoBehaviour
 
     void Start()
     {
-        powerLeft = 99f;
+        powerLeft = 100f;
+        poweredObjects = 1;
     }
 
     private void OnGUI()
@@ -26,6 +33,71 @@ public class PowerManager : MonoBehaviour
 
     void Update()
     {
-        
+        powerLeft -= powerSink * Time.deltaTime;
+        UsePower();
+    }
+
+    public void UsePower()
+    {
+        if(poweredObjects == 1)
+        {
+            powerSink = 0.1f;
+            usageCount[0].SetActive(true);
+            usageCount[1].SetActive(false);
+            usageCount[2].SetActive(false);
+            usageCount[3].SetActive(false);
+            usageCount[4].SetActive(false);
+            usageCount[5].SetActive(false);
+        }
+        else if (poweredObjects == 2)
+        {
+            powerSink = 0.2f;
+            usageCount[0].SetActive(true);
+            usageCount[1].SetActive(true);
+            usageCount[2].SetActive(false);
+            usageCount[3].SetActive(false);
+            usageCount[4].SetActive(false);
+            usageCount[5].SetActive(false);
+        }
+        else if (poweredObjects == 3)
+        {
+            powerSink = 0.3f;
+            usageCount[0].SetActive(true);
+            usageCount[1].SetActive(true);
+            usageCount[2].SetActive(true);
+            usageCount[3].SetActive(false);
+            usageCount[4].SetActive(false);
+            usageCount[5].SetActive(false);
+        }
+        else if (poweredObjects == 4)
+        {
+            powerSink = 0.4f;
+            usageCount[0].SetActive(true);
+            usageCount[1].SetActive(true);
+            usageCount[2].SetActive(true);
+            usageCount[3].SetActive(true);
+            usageCount[4].SetActive(false);
+            usageCount[5].SetActive(false);
+        }
+        else if (poweredObjects == 5)
+        {
+            powerSink = 0.5f;
+            usageCount[0].SetActive(true);
+            usageCount[1].SetActive(true);
+            usageCount[2].SetActive(true);
+            usageCount[3].SetActive(true);
+            usageCount[4].SetActive(true);
+            usageCount[5].SetActive(false);
+        }
+        else if (poweredObjects == 6)
+        {
+            powerSink = 0.6f;
+            usageCount[0].SetActive(true);
+            usageCount[1].SetActive(true);
+            usageCount[2].SetActive(true);
+            usageCount[3].SetActive(true);
+            usageCount[4].SetActive(true);
+            usageCount[5].SetActive(true);
+        }
     }
 }
