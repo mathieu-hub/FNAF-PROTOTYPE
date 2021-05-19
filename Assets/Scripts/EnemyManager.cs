aaa20a7fce907b7671bdd1db8e813245d36b9b1f
mathieu-hub <m.vancappellen@rubika-edu.com>
2021-05-19 11:37:15 +0200

    Add EnemyManager + PositionsPoints Script

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public static EnemyManager Instance;
+
+    public GameObject Freddy;
+    public GameObject Bonnie;
+    public GameObject Chica;
+
+    public float moveSpeed;
+    public Transform actualposition;
+    public Transform targetPosition;
+    public int positionIndex;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
