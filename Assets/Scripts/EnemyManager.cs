5aede3e4a4789f8c7b1745c445a272076649fa1f
mathieu-hub <m.vancappellen@rubika-edu.com>
2021-05-19 20:20:35 +0200

    Need To Get Child In List of Transform

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -10,22 +10,36 @@ public class EnemyManager : MonoBehaviour
     public GameObject freddy;
     public Transform freddyActualPosition;
     public Transform freddyTargetPosition;
+    [Space(5)]
+    public GameObject fPositions;
+    //public Transform[] freddyPositionlist = new Transform[];
     [Range(2f,10f)] public float freddyMoveSpeed;
 
     [Header("CHICCA")]
     public GameObject chicca;
     public Transform chiccaActualPosition;
     public Transform chiccaTargetPosition;
+    [Space(5)]
+    public GameObject cPositions;
+    public List<Transform> chiccaPositionlist = new List<Transform>();
     [Range(2f, 10f)] public float chiccaMoveSpeed;
 
     [Header("BONNIE")]
     public GameObject bonnie;
     public Transform bonnieActualPosition;
     public Transform bonnieTargetPosition;
+    [Space(5)]
+    public GameObject bPositions;
+    public List<Transform> bonniePositionlist = new List<Transform>();
     [Range(2f, 10f)] public float bonnieMoveSpeed;
 
 
     void Start()
+    {
+        //-freddyPositionlist = fPositions.GetComponent<PositionsPoints>().points[transform.childCount];
+    }
+
+    void Initialisation()
     {
         
     }
--- Assets/Scripts/PositionsPoints.cs
+++ Assets/Scripts/PositionsPoints.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class PositionsPoints : MonoBehaviour
 {
-    public static Transform[] points;
+    public List<Transform>[] points;
 
     private void Awake()
     {
-        points = new Transform[transform.childCount];
-
-        for (int i = 0; i < points.Length; i++)
-        {
-            points[i] = transform.GetChild(i);
-        }
+        points = new List<Transform>[transform.childCount];
     }
 }
